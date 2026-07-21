c4aa8cefe7294c9831b11099d21ed762217e0a9c
JUHUI CHOI <dear.ros.mar@gmail.com>
2026-07-22 00:42:26 +0900

    [design] #54 로그인 로고 구성/태그라인 타이포 시안 반영

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,15 +1,19 @@
-import styled from 'styled-components';
+import styled, { useTheme } from 'styled-components';
 import AuthLayout from '@/features/auth/components/AuthLayout';
-import SproutLogo from '@/features/auth/components/SproutLogo';
+import LeafIcon from '@/asset/icons/LeafIcon';
+import wordmark from '@/asset/sprout-wordmark.svg';
 import KakaoLoginButton from '@/features/auth/components/KakaoLoginButton';
 
 function LoginPage() {
+  const theme = useTheme();
+
   return (
     <AuthLayout>
       <Content>
         <TitleArea>
+          <LeafIcon size={59} color={theme.colors.primary} />
           <Tagline>기록이 스펙이 되는 AI 커리어 아카이브 서비스</Tagline>
-          <SproutLogo size="lg" />
+          <Wordmark src={wordmark} alt="SPROUT" />
         </TitleArea>
         <KakaoLoginButton />
       </Content>
@@ -30,13 +34,18 @@ const TitleArea = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  gap: ${({ theme }) => theme.spacing(3)};
+  gap: ${({ theme }) => theme.spacing(2)};
 `;
 
 const Tagline = styled.p`
-  color: ${({ theme }) => theme.colors.primary};
-  font-size: ${({ theme }) => theme.fontSize.xs};
+  color: ${({ theme }) => theme.colors.primaryDark};
+  font-size: 18px;
   font-weight: ${({ theme }) => theme.fontWeight.medium};
+  line-height: ${22 / 18};
+`;
+
+const Wordmark = styled.img`
+  height: 33px;
 `;
 
 export default LoginPage;
