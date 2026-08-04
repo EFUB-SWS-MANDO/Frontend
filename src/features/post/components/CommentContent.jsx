dfe71963d109a620b1936cd7aac98866033ea1ca
JUHUI CHOI <dear.ros.mar@gmail.com>
2026-08-04 21:34:59 +0900

    [fix] #124 비밀 댓글 자물쇠 표시 추가

--- src/features/post/components/CommentContent.jsx
+++ src/features/post/components/CommentContent.jsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import DropdownMenu from '@/components/DropdownMenu/DropdownMenu';
+import LockIcon from '@/asset/icons/LockIcon';
 
 function CommentContent({
   item,
@@ -37,6 +38,7 @@ function CommentContent({
             <Avatar $size={avatarSize} src={item.author.profileImage} alt={`${item.author.name} 프로필`} />
           )}
           <AuthorName>{item.isDeleted ? '(알 수 없음)' : item.author.name}</AuthorName>
+          {!item.isDeleted && item.isPrivate && <LockIcon size={14} label="비밀 댓글" />}
           <CreatedAt>
             {item.createdAt}
             {item.isEdited && ' (수정됨)'}
